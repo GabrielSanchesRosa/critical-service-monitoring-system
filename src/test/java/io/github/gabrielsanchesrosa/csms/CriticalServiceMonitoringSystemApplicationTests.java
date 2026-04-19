2cf284097bb8c5e9e8ababed1e276bc393fe3a8a
Gabriel Sanches <gabrielsanches131202@gmail.com>
2026-04-19 20:34:09 -0300

    chore: initial project structure

---
+++ src/test/java/io/github/gabrielsanchesrosa/csms/CriticalServiceMonitoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gabrielsanchesrosa.csms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriticalServiceMonitoringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
